865d45894eecdf01fcc51595c2193afef037b638
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-01-23 13:55:20 +0700

    handle message when MQ server die (TTL)

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 "use strict";
 
-const { consumerQueue } = require("./src/services/consumer.service");
+const {
+  consumerQueue,
+  consumerToQueueDLX,
+  consumerToQueueNormal,
+} = require("./src/services/consumer.service");
 
 const queueName = "test-queue";
 
-consumerQueue(queueName)
+// consumerQueue(queueName)
+//   .then(() => {
+//     console.log(`Waiting for messages in ${queueName}`);
+//   })
+//   .catch((error) => {
+//     console.error("Error consuming queue", error);
+//   });
+
+consumerToQueueNormal(queueName)
   .then(() => {
-    console.log(`Waiting for messages in ${queueName}`);
+    console.log(`Waiting for messages in ${queueName} normal`);
+  })
+  .catch((error) => {
+    console.error("Error consuming queue", error);
+  });
+
+consumerToQueueDLX(queueName)
+  .then(() => {
+    console.log(`Waiting for messages in ${queueName} DLX`);
   })
   .catch((error) => {
     console.error("Error consuming queue", error);
--- src/services/consumer.service.js
+++ src/services/consumer.service.js
@@ -2,6 +2,12 @@
 
 const { consumerQueue, connectToRabbitMQ } = require("../database/init.rabbit");
 
+const log = console.log;
+
+console.log = function () {
+  log.apply(console, [new Date()].concat(arguments));
+};
+
 const messageService = {
   consumerQueue: async (queueName) => {
     try {
@@ -9,6 +15,62 @@ const messageService = {
       await consumerQueue(channel, queueName);
     } catch (error) {
       console.error("Error consuming queue", error);
+    }
+  },
+
+  // case processing
+  consumerToQueueNormal: async (queueName) => {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+      const notificationQueue = "notification-queue-process"; // assert notification queue
+
+      // const timeout = 15000; // 15s
+      const timeout = 5000; // 5s
+      setTimeout(() => {
+        channel.consume(notificationQueue, (msg) => {
+          console.log(
+            `SEND notification successfully processed: ${msg.content.toString()}`
+          );
+          channel.ack(msg);
+        });
+      }, timeout);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  // case failed processing
+  consumerToQueueDLX: async (queueName) => {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+
+      const notificationExchangeDLX = "notification-exchange-DLX"; //notification direct exchange DLX
+      const notificationRoutingKeyDLX = "notification-routing-key-DLX"; // assert notification queue DLX
+      const notificationQueueHotFix = "notification-queue-hotfix"; // assert notification queue hotfix
+      await channel.assertExchange(notificationExchangeDLX, "direct", {
+        durable: true,
+      });
+      const queueResult = await channel.assertQueue(notificationQueueHotFix, {
+        exclusive: false,
+      });
+      await channel.bindQueue(
+        queueResult.queue,
+        notificationExchangeDLX,
+        notificationRoutingKeyDLX
+      );
+      await channel.consume(
+        queueResult.queue,
+        (msg) => {
+          console.log(
+            `SEND notification failed, pls hot fix: ${msg.content.toString()}`
+          );
+        },
+        {
+          noAck: true,
+        }
+      );
+    } catch (error) {
+      console.error(error);
     }
   },
 };
